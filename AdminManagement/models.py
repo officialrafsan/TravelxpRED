6a19de190f752e92afd55419afd3aae115945809
rafsan <andrafsan@gmail.com>
2022-01-07 22:33:27 +0600

    Admin Management Upgrade

--- AdminManagement/models.py
+++ AdminManagement/models.py
@@ -7,3 +7,6 @@ class Admin(models.Model):
     Admin_name = models.CharField(max_length=100)
     Admin_email = models.CharField(unique=True, max_length=100)
     Admin_contact = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return self.Admin_name
