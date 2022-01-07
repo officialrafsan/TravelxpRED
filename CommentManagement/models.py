0881eb03857d3cdc751d075c99014cb40d0d6b64
rafsan <andrafsan@gmail.com>
2022-01-07 22:21:57 +0600

    Comment Foreign Key

--- CommentManagement/models.py
+++ CommentManagement/models.py
@@ -3,5 +3,10 @@ from django.db import models
 # Create your models here.
 
 
+from UserManagement.models import User
+from PostManagement.models import Post
 class Comment(models.Model):
     Comment_description = models.CharField(max_length=5000)
+
+    User = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    Post = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
