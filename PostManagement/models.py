fb8459c477ce85af0bdc8e9583857d205746472b
rafsan <andrafsan@gmail.com>
2022-01-07 22:14:31 +0600

    User Foreign Key

--- PostManagement/models.py
+++ PostManagement/models.py
@@ -3,8 +3,12 @@ from django.db import models
 # Create your models here.
 
 
+from UserManagement.models import User
+
 class Post(models.Model):
     Post_title = models.CharField(max_length=100)
     Post_location = models.CharField(max_length=100)
     Post_tags = models.CharField(max_length=500)
     Post_description = models.TextField(max_length=1000000)
+
+    User = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
